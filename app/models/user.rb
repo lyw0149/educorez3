b507a6ce763d3fb1a7cbb1c723733b8174954a86
hoyeon <manfromcomputer@gmail.com>
2015-11-27 13:19:24 +0000

    1511272219

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,30 +3,33 @@ class User < ActiveRecord::Base
 	include Authority::UserAbilities
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,:confirmable,
+  devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable,
 					:omniauthable
 	has_many :posts, dependent: :destroy
 	has_many :identities, dependent: :destroy
 	has_many :extras, dependent: :destroy
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image,
+										styles: { medium: "300x300>", thumb: "100x100>" },
+										preserve: true,
+										default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	after_create :set_default_role
   TEMP_EMAIL_PREFIX = 'change@me'
   TEMP_EMAIL_REGEX = /\Achange@me/
- def email_verified?
+ 	def email_verified?
     self.email && self.email !~ TEMP_EMAIL_REGEX
   end
-def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image # assuming the user model has an image
-  end
-end
+	def self.from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+			user.email = auth.info.email
+			user.password = Devise.friendly_token[0,20]
+			user.name = auth.info.name   # assuming the user model has a name
+			user.image = auth.info.image # assuming the user model has an image
+		end
+	end
 	 def self.find_for_oauth(auth, signed_in_resource = nil)
 
     # Get the identity and user if they exist
@@ -44,9 +47,9 @@ end
       # Get the existing user by email if the provider gives us a verified email.
       # If no verified email was provided we assign a temporary email and ask the
       # user to verify it on the next step via UsersController.finish_signup
-      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-      email = auth.info.email if email_is_verified
-      user = User.where(:email => email).first if email
+      # email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+      # email = auth.info.email if email_is_verified
+      # user = User.where(:email => email).first if email
 
       # Create the user if it's a new registration
       if user.nil?
@@ -65,7 +68,7 @@ end
 						password: Devise.friendly_token[0,20]
 						)
 				end
-        user.skip_confirmation!
+      	# user.skip_confirmation!
         user.save!
       end
     end
